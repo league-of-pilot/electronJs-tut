ed4fe276979780fe7688635251cdd47872784a0a
Danh Tran <danh2k@nexondv.com>
2023-12-18 15:48:56 +0700

    init hello ui

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log("hello");
+const { app, BrowserWindow } = require("electron")
+
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600
+  })
+
+  win.loadFile("index.html")
+
+  //   const contents = win.webContents
+  //   console.log(contents)
+}
+
+app.whenReady().then(() => {
+  createWindow()
+})
