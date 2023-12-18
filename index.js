0b983aa24967e14f13b4c776b0b9c73627cb58fc
Danh Tran <danh2k@nexondv.com>
2023-12-18 17:12:59 +0700

    intro preload script

--- index.js
+++ index.js
@@ -1,17 +1,38 @@
 const { app, BrowserWindow } = require("electron")
+const path = require("node:path")
 
 const createWindow = () => {
   const win = new BrowserWindow({
     width: 800,
-    height: 600
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, "preload.js")
+    }
   })
 
   win.loadFile("index.html")
 
+  //   tried to log these in different place to see how its work
   //   const contents = win.webContents
   //   console.log(contents)
 }
 
 app.whenReady().then(() => {
   createWindow()
+
+  app.whenReady().then(() => {
+    createWindow()
+
+    app.on("activate", () => {
+      if (BrowserWindow.getAllWindows().length === 0) {
+        createWindow()
+      }
+    })
+  })
+})
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit()
+  }
 })
